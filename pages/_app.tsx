d9cb834f5db8728bf64cc6227118bac10c0bd6fa
yournewgoodname <suraj.kadiyala@rootfi.in>
2022-11-23 21:26:00 +0530

    go

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,19 +10,11 @@ function MyApp(props) {
   useEffect(() => {
     setMounted(true);
   }, []);
-
   return (
     <>
       {/* <CacheProvider value={clientSideEmotionCache}> */}
       <Head>
-        <link rel="shortcut icon" href="../RootFi Integrate Shortform Favicon.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       {/* <ThemeProvider theme={"dark"}> */}
       <MantineProvider
